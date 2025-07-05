36ae4e9699a884a689104798ca27e302579c129b
Naveenmaddy7081 <85430074+Naveenmaddy7081@users.noreply.github.com>
2025-07-05 21:34:50 +0530

    Add files via upload

---
+++ server2/models/Room.js
@@ -0,0 +1,17 @@
+// server/models/Room.js
+const mongoose = require('mongoose');
+
+const DrawingCommandSchema = new mongoose.Schema({
+  type: String, // 'stroke' or 'clear'
+  data: Object,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const RoomSchema = new mongoose.Schema({
+  roomId: { type: String, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  lastActivity: { type: Date, default: Date.now },
+  drawingData: [DrawingCommandSchema]
+});
+
+module.exports = mongoose.model('Room', RoomSchema);
